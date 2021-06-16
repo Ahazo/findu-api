4e33d35a2b3d32f707c4e0cefe1bc4df45b84a82
Lucca Scarano <lucca.scarano@hotmail.com>
2021-06-15 21:24:26 -0300

    Create find establishment service

--- src/modules/establishment/infra/http/controllers/EstablishmentController.ts
+++ src/modules/establishment/infra/http/controllers/EstablishmentController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe'; 
 
-import FindEstablishmentById from '../../../services/FindEstablishmentByIdService';
+import findEstablishmentService from '../../../services/FindEstablishmentService';
 import CreateEstablishmentService from '../../../services/CreateEstablishmentService';
+import FindEstablishmentByIdService from '../../../services/FindEstablishmentService';
 
 export default class EstablishmentController {
   async createEstablishment(request: Request, response: Response) {
@@ -13,10 +14,24 @@ export default class EstablishmentController {
     return response.status(200).json({ establishment });
   }
 
-  async findEstablishmentById(request: Request, response: Response) {
+  async findById(request: Request, response: Response) {
     const { id } = request.params;
-    const findEstablishmentById = container.resolve(FindEstablishmentById);
-    const establishment = await findEstablishmentById.execute(id);
+    const findEstablishment = container.resolve(FindEstablishmentByIdService);
+    const establishment = await findEstablishment.executeById(id);
+
+    if (!establishment) {
+      return response.status(500).json({
+        message: 'Cant found establishment'
+      });
+    } 
+
+    return response.status(200).json({ establishment });
+  }
+
+  async findByName(request: Request, response: Response) {
+    const { establishment_name } = request.params;
+    const findEstablishment = container.resolve(FindEstablishmentByIdService);
+    const establishment = await findEstablishment.executeByName(establishment_name);
 
     if (!establishment) {
       return response.status(500).json({
--- src/modules/establishment/services/FindEstablishmentService.ts
+++ src/modules/establishment/services/FindEstablishmentService.ts
@@ -11,8 +11,14 @@ export default class FindEstablishmentByIdService {
     private establishmentRepository: IEstablishmentRepository,
   ) {}
 
-  public async execute(establishmentId: string): Promise<Establishment | undefined> {
+  public async executeById(establishmentId: string): Promise<Establishment | undefined> {
     const establishment = await this.establishmentRepository.findById(+establishmentId);
+    console.log(establishment);
+    return establishment;
+  }
+
+  public async executeByName(establishment_name: string): Promise<Establishment | undefined> {
+    const establishment = await this.establishmentRepository.findByName(establishment_name);
     console.log(establishment);
     return establishment;
   }
