b63490fa4171cd568ef8e26150157f23c8498a99
LeviAndre <andrelevi767@gmail.com>
2021-10-14 14:07:52 -0300

    add order status and repositories

---
+++ src/modules/order/repositories/IOrderStatusRepository.ts
@@ -0,0 +1,8 @@
+import ICreateOrderStatusDTO from '../dtos/ICreateOrderStatusDTO';
+import OrderStatus from '../infra/typeorm/entities/OrderStatus';
+
+export default interface IOrderStatusRepository {
+	create(data: ICreateOrderStatusDTO): Promise<OrderStatus>;
+	save(data: OrderStatus): Promise<OrderStatus>;
+	findById(id: number): Promise<OrderStatus | undefined>;
+}
