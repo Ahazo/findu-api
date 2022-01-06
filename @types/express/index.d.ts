97ee595acce99390f2a27ec706ce8a7c2050dd4f
Scarano <scarano.dev@gmail.com>
2022-01-06 12:29:01 -0300

    Adjust types and versions

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/prefer-namespace-keyword */
 /* eslint-disable @typescript-eslint/naming-convention */
-declare namespace Express {
-	interface Request {
-		userId: number;
-	}
 
-	interface IncomingHttpHeaders {
-		token: string;
+declare global {
+	namespace Express {
+		interface Request {
+			userId: number;
+		}
+
+		interface IncomingHttpHeaders {
+			token: string;
+		}
 	}
 }
