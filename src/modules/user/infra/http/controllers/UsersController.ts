bb8c7a4a0e89aaf15491258c9bc3b1af487d19e7
Lucca Scarano <ae_lscarano@idcontractors.net>
2022-09-27 23:18:51 -0300

    Remove influencer level and professional level

--- src/modules/user/infra/http/controllers/UsersController.ts
+++ src/modules/user/infra/http/controllers/UsersController.ts
@@ -5,7 +5,6 @@ import { container } from 'tsyringe';
 import { generateToken } from '../../../../../shared/utils/generateToken';
 import CreateUserService from '../../../services/CreateUserService';
 import FindUserService from '../../../services/FindUserService';
-import FindInfluencerLevelService from '../../../services/influencerLevel/FindInfluencerLevelService';
 
 export default class UsersController {
 	async createUser(request: Request, response: Response): Promise<Response> {
@@ -18,20 +17,8 @@ export default class UsersController {
 			});
 		}
 		try {
-			const findInfluencerLevel = container.resolve(FindInfluencerLevelService);
-			const influencerLevel = await findInfluencerLevel.executeByLevelNumber(1);
-
-			if (!influencerLevel) {
-				return response.status(500).json({
-					error: 'Unable to retrive inital level data.',
-				});
-			}
-
 			const createUser = container.resolve(CreateUserService);
-			const user = await createUser.execute({
-				...userData,
-				level_id: influencerLevel?.id,
-			});
+			const user = await createUser.execute(userData);
 
 			const token = generateToken(user.id);
 
--- src/shared/utils/generateToken.ts
+++ src/shared/utils/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 import authConfig from '../../config/auth';
 
-export const generateToken = (id: number) => {
+export const generateToken = (id: string): string => {
 	const { secret, expiresIn } = authConfig.jwt;
 
 	return jwt.sign({ id }, secret, {
